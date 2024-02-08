6b35952121c85b88afb2645290ace255b227c03e
Hand-of-Doom <kartoshka0ajo@gmail.com>
2024-02-08 19:06:25 +0400

    Add generateImage handler to the routing in processRequest

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -2,10 +2,12 @@ import handleChatGPTMessage from './chatGPTMessage.js';
 import handleChangePersonality from './changePersonality.js'
 import handlePersonalitiesList from './personalitiesList.js'
 import handleSettings from './settings.js'
+import handleGenerateImage from './generateImage.js'
 
 export default {
 	handleChatGPTMessage,
 	handleChangePersonality,
 	handlePersonalitiesList,
-	handleSettings
+	handleSettings,
+	handleGenerateImage
 }
--- src/index.js
+++ src/index.js
@@ -37,6 +37,9 @@ async function processRequest(ctx, db, historyCache, openai) {
     if (buttonListID.includes('change_personality')) {
         return handlers.handleChangePersonality(ctx, db)
     }
+	if (msg.text.body.includes('/image')) {
+		return handlers.handleGenerateImage(ctx, openai)
+	}
     handlers.handleChatGPTMessage(ctx, db, openai) // in background as the top async calls
 }
 
