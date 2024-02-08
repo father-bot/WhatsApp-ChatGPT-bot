bf6b183e85f1b2d77fe1937380ea74e6568d1c97
Hand-of-Doom <kartoshka0ajo@gmail.com>
2024-02-09 00:29:57 +0400

    Add description of /image and /clear commands to the help message

--- src/handlers/help.js
+++ src/handlers/help.js
@@ -1,9 +1,12 @@
 import {Text} from 'whatsapp-api-js/messages'
 
 export default function handleHelp(ctx) {
-	const message = `List of the available commands:
+	const message = `/regen — regenerates the last bot answer
+	
+/clear — clears message history (all the context bot have will be lost, it starts a new dialog)
 
-/regen — regenerates the last bot answer`
+/image $prompt — generates an image based on your prompt
+example: /image sunrise low dim sun a new winter day`
 
 	ctx.reply(new Text(message))
 }
