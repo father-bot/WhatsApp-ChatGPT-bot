1a89e68a0952ecdd11f0ef4ec4c6cc75f2309a17
Hand-of-Doom <kartoshka0ajo@gmail.com>
2024-02-09 17:51:37 +0400

    Add the CREATE TABLE statement of message_history table in setup_tables.sql

--- migrations/setup_tables.sql
+++ migrations/setup_tables.sql
@@ -3,3 +3,11 @@ CREATE TABLE users(
 	phone varchar(15) UNIQUE NOT NULL,
 	personality VARCHAR(35)
 );
+
+CREATE TABLE message_history(
+	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	phone varchar(15) NOT NULL,
+	role varchar(35) NOT NULL,
+	created_at timestamp DEFAULT now(),
+	body varchar(1024) NOT NULL
+);
