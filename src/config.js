cea14a8c7973c154e970165acd8524738e962717
Hand-of-Doom <kartoshka0ajo@gmail.com>
2024-02-14 19:07:11 +0400

    Move all fields to the env variables

--- src/config.js
+++ src/config.js
@@ -8,17 +8,17 @@ function getenv(name) {
 
 export default {
     postgres: {
-        host: 'localhost',
-        port: 5432,
+        host: getenv('POSTGRES_HOST'),
+        port: getenv('POSTGRES_PORT'),
         user: getenv('POSTGRES_USER'),
-        database: 'whatsapp_chatgpt_bot',
+        database: getenv('POSTGRES_DATABASE'),
         password: getenv('POSTGRES_PASSWORD')
     },
     whatsapp: {
         appSecret: getenv('WHATSAPP_APP_SECRET'),
         apiKey: getenv('WHATSAPP_API_KEY'),
         webhookVerifyToken: getenv('WHATSAPP_WEBHOOK_VERIFY_TOKEN'),
-        webhookPort: 8080
+        webhookPort: getenv('WHATSAPP_WEBHOOK_PORT')
     },
     openai: {
         apiKey: getenv('OPEN_AI_API_KEY')
