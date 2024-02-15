7f5527755677cd6c9f3f476406e08a3e01241fb3
Hand-of-Doom <kartoshka0ajo@gmail.com>
2024-02-15 16:37:31 +0400

    The description of /regen command has been removed from the help message (cos that command was removed a few days ago)

--- src/handlers/help.js
+++ src/handlers/help.js
@@ -1,9 +1,7 @@
 import {Text} from 'whatsapp-api-js/messages'
 
 export default function handleHelp(ctx) {
-	const message = `/regen — regenerates the last bot answer
-	
-/clear — clears message history (all the context bot have will be lost, it starts a new dialog)
+	const message = `/clear — clears message history (all the context bot have will be lost, it starts a new dialog)
 
 /image $prompt — generates an image based on your prompt
 example: /image sunrise low dim sun a new winter day`
