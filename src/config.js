0c4889bf408d69f3c74d1286dfcec9dfe36cd157
Hand-of-Doom <kartoshka0ajo@gmail.com>
2024-03-01 22:40:46 +0400

    Removed the section whatsapp from config.js
    cause it's not an official whatsapp app and the token, app secret and the other stuff is not needed

--- src/config.js
+++ src/config.js
@@ -14,12 +14,6 @@ export default {
         database: getenv('POSTGRES_DATABASE'),
         password: getenv('POSTGRES_PASSWORD')
     },
-    whatsapp: {
-        appSecret: getenv('WHATSAPP_APP_SECRET'),
-        apiKey: getenv('WHATSAPP_API_KEY'),
-        webhookVerifyToken: getenv('WHATSAPP_WEBHOOK_VERIFY_TOKEN'),
-        webhookPort: getenv('WHATSAPP_WEBHOOK_PORT')
-    },
     openai: {
         apiKey: getenv('OPEN_AI_API_KEY')
     }
